a0a8842cb3a67d8b1c8046127c3017b228d6ce6a
vladimir.gorea <vladimir.gorea@lionstep.com>
2024-03-15 16:03:53 +0800

    Up version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="secret_lib",
-    version="0.1",
+    version="0.1.1",
     author="vladimir gorea",
     author_email="vladimir@smileservices.dev",
     description="A wrapper for different configuration/secret sources.",
